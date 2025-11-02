c724437f55fd993646be1b5a00919e1e9a51db31
Nare <nareazaryan06@gmail.com>
2025-11-02 10:42:48 +0400

    48 task added

--- matrixclass.cpp
+++ matrixclass.cpp
@@ -3,57 +3,76 @@ using namespace std;
 
 class Matrix {
 private:
-    int rows;          
-    int cols;          
-    int data[10][10];  
+  int rows;
+  int cols;
+  int data[10][10];
 
 public:
-
-    Matrix(int r, int c);
-    void Input();
-    void Display() const;
+  Matrix(int r, int c);
+  Matrix(const Matrix &other);
+  Matrix &operator=(const Matrix &other);
+  void Input();
+  void Display() const;
 };
 
 Matrix::Matrix(int r, int c) {
-    rows = r;
-    cols = c;
+  rows = r;
+  cols = c;
+}
+
+Matrix::Matrix(const Matrix &other) {
+  rows = other.rows;
+  cols = other.cols;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      data[i][j] = other.data[i][j];
+    }
+  }
+}
+
+Matrix &Matrix::operator=(const Matrix &other) {
+  if (this != &other) {
+    rows = other.rows;
+    cols = other.cols;
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        data[i][j] = other.data[i][j];
+      }
+    }
+  }
+  return *this;
 }
 
 void Matrix::Input() {
-    cout << "Enter elements of matrix (" << rows << "x" << cols << "):\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << "a[" << i << "][" << j << "] = ";
-            cin >> data[i][j];
-        }
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      cout << "a[" << i << "][" << j << "]: ";
+      cin >> data[i][j];
     }
+  }
 }
 
 void Matrix::Display() const {
-    cout << "Matrix (" << rows << "x" << cols << "):\n";
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            cout << data[i][j] << " ";
-        }
-        cout << endl;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      cout << data[i][j] << " ";
     }
+    cout << endl;
+  }
 }
 
-
-
-
 int main() {
-    Matrix A(2, 2), B(2, 2);
+  Matrix A(2, 2), B(2, 2);
 
-    cout << "Enter first matrix:\n";
-    A.Input();
+  cout << "Enter first matrix:" << endl;
+  A.Input();
 
-    cout << "\nEnter second matrix:\n";
-    B.Input();
+  cout << endl << "Enter second matrix:" << endl;
+  B.Input();
 
-    cout << "\nMatrix A:\n";
-    A.Display();
+  cout << endl << "Matrix A:" << endl;
+  A.Display();
 
-    cout << "\nMatrix B:\n";
-    B.Display();
+  cout << endl << "Matrix B:" << endl;
+  B.Display();
 }
