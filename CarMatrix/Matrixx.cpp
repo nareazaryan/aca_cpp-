29da92b0742fe2588c52dc4da8ce1c94bd3e9d19
Nare <nareazaryan06@gmail.com>
2025-11-07 18:56:53 +0400

    Matrix class which contains Car class

--- CarMatrix/Matrixx.cpp
+++ CarMatrix/Matrixx.cpp
@@ -33,6 +33,33 @@ Matrix::Matrix(const Matrix &other) : m_size(other.m_size) {
   }
 }
 
+void Matrix::Initialize() {
+  string names[] = {"BMW", "Toyota", "Audi", "Mercedes", "Nissan"};
+  string models[] = {"X5", "Corolla", "A6", "C-Class", "Altima"};
+  string colors[] = {"Black", "White", "Silver", "Blue", "Red"};
+  string engines[] = {"Diesel", "Hybrid", "Petrol", "Electric", "Gas"};
+
+  int index = 0;
+
+  for (int i = 0; i < m_size; ++i) {
+    for (int j = 0; j < m_size; ++j) {
+      if (m_data[i][j] != nullptr) {
+        delete m_data[i][j];
+      }
+
+      string name = names[index % 5];
+      string model = models[index % 5];
+      string color = colors[index % 5];
+      string engine = engines[index % 5];
+      int year = 2020 + (index % 5);
+
+      m_data[i][j] = new Car(name, model, year, color, engine);
+
+      index++;
+    }
+  }
+}
+
 Matrix &Matrix::operator=(const Matrix &other) {
   if (this == &other)
     return *this;
@@ -58,3 +85,27 @@ Matrix &Matrix::operator=(const Matrix &other) {
   return *this;
 }
 
+void Matrix::pasteCar(const Car &c, int row, int col) {
+  if (row < 0 || row >= m_size || col < 0 || col >= m_size) {
+    cout << "Invalid position!" << endl;
+    return;
+  }
+
+  if (m_data[row][col] != nullptr)
+    delete m_data[row][col];
+
+  m_data[row][col] = new Car(c);
+}
+
+void Matrix::Print() const {
+  for (int i = 0; i < m_size; ++i) {
+    for (int j = 0; j < m_size; ++j) {
+      if (m_data[i][j] != nullptr) {
+        cout << "Position [" << i << "][" << j << "]:" << endl;
+        m_data[i][j]->DisplayInfo();
+      } else {
+        cout << "Position [" << i << "][" << j << "]: [ Empty ]" << endl;
+      }
+    }
+  }
+}
---
+++ CarMatrix/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Car.h"
+#include "Matrixx.h"
+using namespace std;
+
+
+int main() {
+    Matrix m(2);
+    m.Initialize();   
+    m.Print();       
+    return 0;
+}
